8ad82471219a8511e285923e2d1d1e7bbfcb1d33
Hoang Quoc Trung <hoang.quoc.trung@sun-asterisk.com>
2019-09-17 10:58:33 +0700

    use readBytes() + show result

--- app/src/main/java/com/miller/futurechat/MainActivity.kt
+++ app/src/main/java/com/miller/futurechat/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.miller.futurechat
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.miller.mockretrofitinterceptor.MockInterceptor
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -19,6 +19,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        callMockApi()
+    }
+
+    private fun callMockApi() {
         val retrofit = Retrofit.Builder()
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
@@ -40,10 +44,11 @@ class MainActivity : AppCompatActivity() {
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
                 {
-                    Log.d("------>"," : ${it.id}")
+                    textId.text = it.id.toString()
                 },
                 {
                     it.printStackTrace()
+                    textId.text = it.localizedMessage
                 }
             ).apply {
                 CompositeDisposable().add(this)
