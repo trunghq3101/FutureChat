0888ced1fd5e9693b76029550735a35efdc76bb0
Trung P <trunghq3101@gmail.com>
2019-09-18 22:35:25 +0700

    add sign in

--- app/src/main/java/com/miller/futurechat/MainActivity.kt
+++ app/src/main/java/com/miller/futurechat/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.miller.futurechat
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.iid.FirebaseInstanceId
+import com.miller.authentication.FirebaseUIActivity
 
 @ExperimentalStdlibApi
 class MainActivity : AppCompatActivity() {
@@ -23,5 +25,7 @@ class MainActivity : AppCompatActivity() {
             .addOnFailureListener {
                 it.printStackTrace()
             }
+
+        startActivity(Intent(this, FirebaseUIActivity::class.java))
     }
 }
